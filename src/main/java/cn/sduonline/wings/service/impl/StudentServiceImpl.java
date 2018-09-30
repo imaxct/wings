c2b23dfa3b11a6d604cc252a29e4884de773642f
imaxct <imaxct@foxmail.com>
2018-09-30 15:39:31 +0800

    更新

--- src/main/java/cn/sduonline/wings/service/impl/StudentServiceImpl.java
+++ src/main/java/cn/sduonline/wings/service/impl/StudentServiceImpl.java
@@ -2,6 +2,8 @@ package cn.sduonline.wings.service.impl;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.support.TransactionTemplate;
@@ -12,6 +14,7 @@ import cn.sduonline.wings.dao.mapper.SelectMapper;
 import cn.sduonline.wings.dao.mapper.SettingMapper;
 import cn.sduonline.wings.dao.mapper.StudentMapper;
 import cn.sduonline.wings.model.*;
+import cn.sduonline.wings.model.condition.CourseCondition;
 import cn.sduonline.wings.model.condition.SelectCondition;
 import cn.sduonline.wings.model.condition.SettingCondition;
 import cn.sduonline.wings.model.condition.StudentCondition;
@@ -31,6 +34,8 @@ public class StudentServiceImpl implements StudentService {
 	private final CourseMapper courseMapper;
 
 	private final TransactionTemplate transactionTemplate;
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(StudentServiceImpl.class);
 
 	@Autowired
 	public StudentServiceImpl(StudentMapper studentMapper, SettingMapper settingMapper, SelectMapper selectMapper,
@@ -74,7 +79,7 @@ public class StudentServiceImpl implements StudentService {
 		SettingCondition condition = new SettingCondition();
 		condition.setSettingName(SettingName.ANNOUNCEMENT);
 		List<Setting> list = settingMapper.selectByCondition(condition);
-		if (CollectionUtils.isEmpty(list) || list.size() != 1) {
+		if (CollectionUtils.isEmpty(list)) {
 			Setting setting = new Setting();
 			setting.setSettingName(SettingName.ANNOUNCEMENT);
 			setting.setSettingValue("无公告");
@@ -140,6 +145,7 @@ public class StudentServiceImpl implements StudentService {
 
 				return Result.ok(null);
 			} catch (Exception e) {
+				LOGGER.error("selectCourse", e);
 				t.setRollbackOnly();
 				return Result.err("选课失败, 重复选课或课余量不足", null);
 			}
@@ -149,11 +155,38 @@ public class StudentServiceImpl implements StudentService {
 
 	@Override
 	public Result deselectCourse(Long studentId, Long courseId) {
-		return null;
+		SelectCondition selectCondition = new SelectCondition();
+		selectCondition.setStudentId(studentId);
+		selectCondition.setCourseId(courseId);
+		List<Select> selectList = selectMapper.selectByCondition(selectCondition);
+		if (CollectionUtils.isEmpty(selectList)) {
+			return Result.err("退选失败, 未选择该课程", null);
+		}
+		Student student = studentMapper.selectByPrimaryKey(studentId);
+		transactionTemplate.execute((t) -> {
+			try {
+				PoorLevelEnum poorLevel = PoorLevelEnum.valueOf(student.getPoorLevel());
+				Course course = courseMapper.selectByPrimaryKeyForUpdate(courseId);
+				if (poorLevel == PoorLevelEnum.NOT_POOR) {
+					course.setNotPoorNum(course.getNotPoorNum() + 1);
+				}
+				course.setAvailableNum(course.getAvailableNum() + 1);
+				courseMapper.updateByPrimaryKeySelective(course);
+				selectMapper.deleteByPrimaryKey(selectList.get(0).getId());
+				return Result.ok(null);
+			} catch (Exception e) {
+				LOGGER.error("deselectCourse", e);
+				t.setRollbackOnly();
+				return Result.err("退选失败, 请稍候", null);
+			}
+		});
+		return Result.err("退选失败", null);
 	}
 
 	@Override
+	@SuppressWarnings("unchecked")
 	public Result<List<Course>> getCourseList() {
-		return null;
+		List<Course> courseList = courseMapper.selectByCondition(new CourseCondition());
+		return Result.ok(courseList);
 	}
 }
--- src/main/java/cn/sduonline/wings/vo/Result.java
+++ src/main/java/cn/sduonline/wings/vo/Result.java
@@ -1,9 +1,9 @@
 package cn.sduonline.wings.vo;
 
+import java.io.Serializable;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
-import java.io.Serializable;
 
 /**
  * Created by imaxct on 18-9-26.
@@ -11,15 +11,15 @@ import java.io.Serializable;
 @Data
 @AllArgsConstructor
 public class Result<T> implements Serializable {
-    private boolean ok;
-    private String msg;
-    private T data;
+	private boolean ok;
+	private String msg;
+	private T data;
 
-    public static Result ok(Object data) {
-        return new Result<>(true, null, data);
-    }
+	public static <T> Result ok(T data) {
+		return new Result<>(true, null, data);
+	}
 
-    public static Result err(String msg, Object data) {
-        return new Result<>(false, msg, data);
-    }
+	public static Result err(String msg, Object data) {
+		return new Result<>(false, msg, data);
+	}
 }
